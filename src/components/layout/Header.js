1aa2876b8f0a95a3fbd4064875a099bf26d327ea
Martin Hunt <martinseanhunt@gmail.com>
2021-03-09 16:45:11 +0000

    Header

---
+++ src/components/layout/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+// TODO: Build menu from pages in gql query
+// TODO: Style active links etc
+// TODO: Logo and favicon
+
+export function Header() {
+  return (
+    <StyledHeader>
+      <span>👋</span>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+    </StyledHeader>
+  )
+}
+
+// TODO: values from theme
+const StyledHeader = styled.header`
+  margin-bottom: 64px;
+`
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { createGlobalStyle } from 'styled-components'
 
+import { Header } from './Header'
+
 export function Layout({ children }) {
   return (
     <>
       <GlobalStyle />
       <div>
+        <Header />
         <main>{children}</main>
       </div>
     </>
   )
 }
 
+// TODO: set up theme for colours, font sizes etc
 const GlobalStyle = createGlobalStyle`
   html,
   body {
@@ -19,5 +23,13 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     font-family: -apple-system, Roboto, sans-serif, serif;
     color: #1c1b21;
+  }
+
+  body {
+    padding: 50px;
+  }
+
+  * {
+    box-sizing: border-box;
   }
 `
