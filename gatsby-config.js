da529b2b887a99050035d6c2737a87a90d91b829
Martin Hunt <martinseanhunt@gmail.com>
2021-03-09 17:25:46 +0000

    tweak folders structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,29 @@ module.exports = {
     title: `Martin Hunt`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+            },
+          },
+        ],
+      },
+    },
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
   ],
 }
