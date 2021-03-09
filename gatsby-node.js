3ead2d456e76aef8687ebf3d7fb252dbdb1eb428
Martin Hunt <martinseanhunt@gmail.com>
2021-03-09 21:11:56 +0000

    create basic blog post template, set up blog admin, add slugs to markdownremark feild and generate static pages for non external blog posts

---
+++ gatsby-node.js
@@ -0,0 +1,66 @@
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+// Add slug as field to markdownRemark
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: `src/content`,
+      trailingSlash: false,
+    })
+
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+// Create pages for blog posts that don't have an external url
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
+  const BlogPost = require.resolve(`./src/templates/BlogPost.js`)
+
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        filter: {
+          frontmatter: {
+            templateKey: { eq: "blog-post" }
+            externalUrl: { eq: null }
+          }
+        }
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: BlogPost,
+      context: {
+        // additional data can be passed via context
+        slug: node.fields.slug,
+      },
+    })
+  })
+}
---
+++ src/templates/BlogPost.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import { Helmet } from 'react-helmet'
+
+export default function BlogPost({ data }) {
+  const { markdownRemark, site } = data
+  const { frontmatter, html } = markdownRemark
+
+  return (
+    <>
+      <Helmet
+        title={`${frontmatter.title} - ${site.siteMetadata.title}`}
+        defer={false}
+      />
+
+      <div className="blog-post-container">
+        <div className="blog-post">
+          <h1>{frontmatter.title}</h1>
+          <h2>{frontmatter.date}</h2>
+          <div
+            className="blog-post-content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export const PostDetail = graphql`
+  fragment PostDetail on MarkdownRemark {
+    fields {
+      slug
+    }
+    id
+    rawMarkdownBody
+    timeToRead
+    html
+    frontmatter {
+      date
+      title
+    }
+    excerpt
+  }
+`
+
+export const query = graphql`
+  query($slug: String!) {
+    ...SiteMeta
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      ...PostDetail
+    }
+  }
+`
