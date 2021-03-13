61a5b7dfed0aa892190adf62cc349586c5b6b8a8
Martin Hunt <martinseanhunt@gmail.com>
2021-03-13 20:41:49 +0000

    debugging production

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import { Helmet } from 'react-helmet'
 
 import { Terminal } from '../components/Terminal'
 import { BlogPostList } from '../components/BlogPostList'
-import { WorkHistory } from '../components/WorkHistory'
 
 export default function Index({ data }) {
   const { site, markdownRemark: page } = data
@@ -16,7 +15,6 @@ export default function Index({ data }) {
         defer={false}
       />
       <Terminal />
-      <WorkHistory />
       <BlogPostList />
     </>
   )
